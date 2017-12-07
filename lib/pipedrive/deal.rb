28e03e0c03c9db91be49c72322c70098b5fd0a7a
Alexey Terekhov <alexey.terekhov.tap349@gmail.com>
2017-12-07 16:52:29 +0300

    add remove_participant

--- lib/pipedrive/deal.rb
+++ lib/pipedrive/deal.rb
@@ -9,16 +9,21 @@ module Pipedrive
     def products
       Product.all(get "#{resource_path}/#{id}/products")
     end
-    
+
     def add_participant(opts = {})
       res = post "#{resource_path}/#{id}/participants", :body => opts
+      res.success? ? true : bad_response(res,opts)
+    end
+
+    def remove_participant(opts = {})
+      res = delete "#{resource_path}/#{id}/participants", :body => opts
       res.success? ? true : bad_response(res,opts)
     end
 
     def participants
       Person.all(get "#{resource_path}/#{id}/participants")
     end
-    
+
     def remove_product product_attachment_id
       res = delete "#{resource_path}/#{id}/products", { :body => { :product_attachment_id => product_attachment_id } }
       res.success? ? nil : bad_response(res,product_attachment_id)
